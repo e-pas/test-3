219c74a5e02e990d6b6ef7d4ef4676acef8c5f1a
e-pas <epasekunov@gmail.com>
2023-03-04 16:59:26 +0200

    ABS func tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test-3
+
+go 1.20
---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestAbs(t *testing.T) {
+	tests := []struct {
+		name   string
+		values []float64
+		want   []float64
+	}{
+		{
+			name:   "First values",
+			values: []float64{-3, 3, -2.000001, -0.000000003},
+			want:   []float64{3, 3, 2.000001, 0.000000003},
+		},
+	}
+
+	for _, tt := range tests {
+		for ik, _ := range tt.values {
+			t.Run(tt.name, func(t *testing.T) {
+				if res := Abs(tt.values[ik]); res != tt.want[ik] {
+					t.Errorf("Error in test: %s. Waited: %f. Got: %f", tt.name, tt.want[ik], res)
+				}
+			})
+		}
+	}
+}
